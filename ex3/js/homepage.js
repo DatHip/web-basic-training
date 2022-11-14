be614c2b449cd8cf8d81a40b01cb5fddd04c44b7
DatHip <83833987+DatHip@users.noreply.github.com>
2022-11-14 16:54:41 +0700

    update

--- ex3/js/homepage.js
+++ ex3/js/homepage.js
@@ -1,44 +1,43 @@
-const btnMore = document.querySelector(".more")
-const btnMenu = document.querySelector(".btn-menu")
-const listMenu = document.querySelector(".list_menu")
-const items = document.querySelectorAll(".item-city")
-const cityCards = document.querySelectorAll(".city-card")
+const btnMore = document.querySelector(".more");
+const btnMenu = document.querySelector(".btn-menu");
+const listMenu = document.querySelector(".list_menu");
+const items = document.querySelectorAll(".item-city");
+const cityCards = document.querySelectorAll(".city-card");
 // event
 
 btnMore.addEventListener("click", (e) => {
-   if (!btnMore.classList.contains("active")) {
-      btnMore.classList.add("active")
-   } else {
-      btnMore.classList.remove("active")
-   }
-})
+  if (!btnMore.classList.contains("active")) {
+    btnMore.classList.add("active");
+  } else {
+    btnMore.classList.remove("active");
+  }
+});
 
 btnMenu.addEventListener("click", () => {
-   if (!listMenu.classList.contains("active")) {
-      listMenu.classList.add("active")
-   } else {
-      listMenu.classList.remove("active")
-   }
-})
-
-const handleClick = (e, index) => {}
+  if (!listMenu.classList.contains("active")) {
+    listMenu.classList.add("active");
+  } else {
+    listMenu.classList.remove("active");
+  }
+});
 
 items.forEach((e, index) => {
-   let cityCard = cityCards[index]
+  let cityCard = cityCards[index];
 
-   if (index === 6 || index === 7) {
-      cityCard = cityCards[index - 2]
-      console.log(cityCard)
-   }
+  if (index === 6 || index === 7) {
+    cityCard = cityCards[index - 2];
+  }
 
-   e.addEventListener("click", () => {
-      document.querySelector(".item-city.active").classList.remove("active")
-      document.querySelector(".city-card.active").classList.remove("active")
+  e.addEventListener("click", () => {
+    document.querySelector(".item-city.active").classList.remove("active");
+    document.querySelector(".city-card.active").classList.remove("active");
 
-      e.classList.add("active")
-      cityCard.classList.add("active")
-      if (listMenu.classList.contains("active")) {
-         listMenu.classList.remove("active")
-      }
-   })
-})
+    e.classList.add("active");
+
+    cityCard.classList.add("active");
+
+    if (listMenu.classList.contains("active")) {
+      listMenu.classList.remove("active");
+    }
+  });
+});
--- ex5/js/layout.js
+++ ex5/js/layout.js
@@ -1,26 +1,26 @@
-let btnClose = document.querySelector(".btn-close")
-let btnMenu = document.querySelector(".btn-menu")
-let slider = document.querySelector(".body")
-let container = document.querySelector(".content")
+let btnClose = document.querySelector(".btn-close");
+let btnMenu = document.querySelector(".btn-menu");
+let slider = document.querySelector(".body");
+let container = document.querySelector(".content");
+let btnMenus = document.querySelectorAll(".menu-title");
 
 btnMenu.addEventListener("click", () => {
-   slider.classList.add("active")
-})
-btnClose.addEventListener("click", () => {
-   slider.classList.remove("active")
-})
+  slider.classList.add("active");
+});
 
-let btnMenus = document.querySelectorAll(".menu-title")
+btnClose.addEventListener("click", () => {
+  slider.classList.remove("active");
+});
 
 btnMenus.forEach((e) => {
-   e.addEventListener("click", (e) => {
-      document.querySelector(".menu-title.active").classList.remove("active")
+  e.addEventListener("click", (e) => {
+    document.querySelector(".menu-title.active").classList.remove("active");
 
-      e.target.classList.add("active")
-      container.innerHTML = "Demo Container " + e.target.innerText
+    e.target.classList.add("active");
+    container.innerHTML = "Demo Container " + e.target.innerText;
 
-      if (slider.classList.contains("active")) {
-         slider.classList.remove("active")
-      }
-   })
-})
+    if (slider.classList.contains("active")) {
+      slider.classList.remove("active");
+    }
+  });
+});
