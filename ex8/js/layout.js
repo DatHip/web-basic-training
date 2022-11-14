5e392e4d3dcb93ec06c49819979fb84b8ccbdc8b
DatHip <83833987+DatHip@users.noreply.github.com>
2022-11-14 10:42:25 +0700

    update

--- ex8/js/layout.js
+++ ex8/js/layout.js
@@ -1,25 +1,25 @@
-let btnClose = document.querySelector(".btn-close")
-let btnMenu = document.querySelector(".button-menu")
-let slider = document.querySelector(".sider")
-let container = document.querySelector(".content")
+let btnClose = document.querySelector(".btn-close");
+let btnMenu = document.querySelector(".button-menu");
+let slider = document.querySelector(".sidebar");
+let container = document.querySelector(".content");
 btnMenu.addEventListener("click", () => {
-   slider.classList.add("active")
-})
+  slider.classList.add("active");
+});
 btnClose.addEventListener("click", () => {
-   slider.classList.remove("active")
-})
+  slider.classList.remove("active");
+});
 
-let btnMenus = document.querySelectorAll(".menu-title")
+let btnMenus = document.querySelectorAll(".menu-title");
 
 btnMenus.forEach((e) => {
-   e.addEventListener("click", (e) => {
-      document.querySelector(".menu-title.active").classList.remove("active")
+  e.addEventListener("click", (e) => {
+    document.querySelector(".menu-title.active").classList.remove("active");
 
-      e.target.classList.add("active")
-      container.innerHTML = "Demo Container " + e.target.innerText
+    e.target.classList.add("active");
+    container.innerHTML = "Demo Container " + e.target.innerText;
 
-      if (slider.classList.contains("active")) {
-         slider.classList.remove("active")
-      }
-   })
-})
+    if (slider.classList.contains("active")) {
+      slider.classList.remove("active");
+    }
+  });
+});
